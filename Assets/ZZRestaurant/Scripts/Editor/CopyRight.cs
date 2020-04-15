e54a9ad981e77973912acf5c0133467bf23f4d0a
Big-River-zm <63436838+Big-River-zm@users.noreply.github.com>
2020-04-15 23:21:40 +0800

    UGUIGroupHelperLearning

---
+++ Assets/ZZRestaurant/Scripts/Editor/CopyRight.cs
@@ -0,0 +1,33 @@
+﻿//=====================================================
+// - FileName:      CopyRight.cs
+// - Created:       #AuthorName#
+// - CreateTime:      #CreateTime#
+// - Email:         #AuthorEmail#
+// - Description:   
+// -  (C) Copyright 2020, SlagObjectPool, Inc.
+// -  All Rights Reserved.
+//======================================================
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Copyright : UnityEditor.AssetModificationProcessor
+{
+    private const string AuthorName = "BeanG";
+    private const string AuthorEmail = "gongjied@hotmail.com";
+
+    private const string DateFormat = "yyyy/MM/dd HH:mm:ss";
+    private static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            string allText = File.ReadAllText(path);
+            allText = allText.Replace("#AuthorName#", AuthorName);
+            allText = allText.Replace("#AuthorEmail#", AuthorEmail);
+            allText = allText.Replace("#CreateTime#", System.DateTime.Now.ToString(DateFormat));
+            File.WriteAllText(path, allText);
+            UnityEditor.AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/ZZRestaurant/Scripts/UI/UGUIForm.cs
@@ -0,0 +1,22 @@
+//=====================================================
+// - FileName:      UGUIForm.cs
+// - Created:       BeanG
+// - CreateTime:      2020/04/15 23:00:34
+// - Email:         gongjied@hotmail.com
+// - Description:   
+// -  (C) Copyright 2020, SlagObjectPool, Inc.
+// -  All Rights Reserved.
+//======================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace ZZ
+{
+	public class UGUIForm : UIFormLogic
+    {
+	
+	    
+	}
+}
